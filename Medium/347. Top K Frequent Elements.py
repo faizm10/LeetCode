e560e6052b167fa0b79a92bc39fb5d25b6b5724a
faizm10 <faizmustansar10@gmail.com>
2025-09-05 11:12:53 -0400

    commit

--- Medium/347. Top K Frequent Elements.py
+++ Medium/347. Top K Frequent Elements.py
@@ -1,2 +1,12 @@
+# Given an integer array nums and an integer k, return the k most frequent elements within the array.
+#Input: nums = [1,2,2,3,3,3], k = 2
+#Output: [2,3]
+
 def topKFrequent(nums, k):  
-    return 0
+    unique_numbers = set()
+    list = []
+    for num in nums:
+        if num not in unique_numbers:  # Check if the number is already in the set
+            unique_numbers.add(num)    # Add to the set to mark it as seen
+            list.append(num)        # Add to the list
+    return list
