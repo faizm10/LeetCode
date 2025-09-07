45282c523b4632b7c475fc0cf66df79a3fc2e9c2
faizm10 <faizmustansar10@gmail.com>
2025-09-06 22:11:47 -0400

    finished

--- Medium/encode-decode-strings.py
+++ Medium/encode-decode-strings.py
@@ -1,7 +1,19 @@
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        return "#".join(strs)
+        return ''.join(f"{len(s)}#{s}" for s in strs)
+
 
     def decode(self, s: str) -> List[str]:
-        return s.split("#")
+        res = []
+        i = 0
+        n = len(s)
+        while i < n:
+            j = i
+            while j < n and s[j] != '#':
+                j += 1
+            length = int(s[i:j])  # parse length
+            j += 1                # skip '#'
+            res.append(s[j:j+length])
+            i = j + length
+        return res
