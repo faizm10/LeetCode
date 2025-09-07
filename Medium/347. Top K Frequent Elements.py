db045bdf8d47cc0792414ed37e161d1fe56b8a0d
faizm10 <faizmustansar10@gmail.com>
2025-09-06 21:48:41 -0400

    finished

--- Medium/347. Top K Frequent Elements.py
+++ Medium/347. Top K Frequent Elements.py
@@ -1,12 +1,16 @@
 # Given an integer array nums and an integer k, return the k most frequent elements within the array.
 #Input: nums = [1,2,2,3,3,3], k = 2
 #Output: [2,3]
-
-def topKFrequent(nums, k):  
-    unique_numbers = set()
-    list = []
-    for num in nums:
-        if num not in unique_numbers:  # Check if the number is already in the set
-            unique_numbers.add(num)    # Add to the set to mark it as seen
-            list.append(num)        # Add to the list
-    return list
+class Solution:
+    def topKFrequent(self, nums, k):
+        
+        freq = Counter(nums) # init the freq counter 
+        buckets = [[] for _ in range(len(nums) + 1)] # create the buckets
+        for val, c in freq.items():
+            buckets[c].append(val) # fill the buckets
+        res = []
+        for i in range(len(buckets) - 1, 0, -1): # iterate the buckets in reverse order
+            for v in buckets[i]:
+                res.append(v)
+                if len(res) == k:
+                    return res
