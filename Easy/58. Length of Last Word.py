bc44efb915331fc4227716a3fd401f63988386f9
faizm10 <faizmustansar10@gmail.com>
2025-10-10 14:27:34 -0400

    finished

--- Easy/58. Length of Last Word.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        lol = s.split(" ")
-        lol = [word for word in lol if word != ""]
-        return len(lol[-1])
-        
--- Medium/49. Group Anagrams.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list) # creates list if empty, otherwise add to the list
-        for s in strs:
-            sortedS = ''.join(sorted(s)) # breaks down each letter and then sorts it
-            res[sortedS].append(s)
-        return list(res.values())
