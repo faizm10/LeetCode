ba699428e50a7d40518fac14bd8f10de52fa5b84
faizm10 <faizmustansar10@gmail.com>
2025-10-10 11:03:57 -0400

    217. contains duplicate

--- Easy/217. Contains Duplicate.py
+++ Easy/217. Contains Duplicate.py
@@ -1,17 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # Create an empty set to keep track of numbers in nums
-        seen = set()
-        
-        # Loop through each number in the list
-        for num in nums:
-            # Check if the number is already in the set
-            if num in seen:  
-                # If the number is in the set, it's a duplicate, so return True
-                return True
-            
-            # If the number is not in the set, add it to the set
-            seen.add(num)
-        
-        # If no duplicates are found after checking all numbers, return False
+        arr = set() # init the set
+        for i in nums: # checking every number in nums (list)
+            if i in arr: # if index is in the set, it already exists
+                return True 
+            arr.add(i) # if not, it is unique so we add it to the set
         return False
