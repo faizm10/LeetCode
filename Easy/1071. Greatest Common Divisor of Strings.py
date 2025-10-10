4f47bc4fb5eed4c10661d8c07937fdd24fa866ce
faizm10 <faizmustansar10@gmail.com>
2025-10-10 14:27:39 -0400

    finished

--- Easy/1071. Greatest Common Divisor of Strings.py
+++
@@ -1,8 +0,0 @@
-import math
-class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        d = math.gcd(len(str1), len(str2))
-        if (str1[:d] == str2[:d]):
-            return str1[:d]
-        else:
-            return ""
