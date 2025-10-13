710765479f3581b3a391e206349139b5dda89c98
faizm10 <faizmustansar10@gmail.com>
2025-10-12 22:50:53 -0400

    done

---
+++ a-Notes/dsa.py
@@ -0,0 +1,16 @@
+def important_dsa_list():
+    """
+    This function generates a list of important data structures and algorithms that are commonly used in leetcoding.
+    """
+    dsa_list = [
+        {"data_structure": "Arrays", "algorithm": "Two Pointers", "example": "Given an array of integers, return the pairs of two elements whose sum is equal to the given target sum"},
+        {"data_structure": "Linked Lists", "algorithm": "Reverse Linked List", "example": "Write a program to reverse a singly linked list"},
+        {"data_structure": "Stacks", "algorithm": "Valid Parentheses", "example": "Given a string of parentheses, determine if the string is valid or not"},
+        {"data_structure": "Queues", "algorithm": "Breadth First Search", "example": "Write a program to traverse a graph using Breadth First Search"},
+        {"data_structure": "Trees", "algorithm": "Inorder Traversal", "example": "Write a program to traverse a tree using inorder traversal"},
+        {"data_structure": "Hash Tables", "algorithm": "Find the Duplicate Number", "example": "Given an array of integers, find the duplicate number"},
+        {"data_structure": "Graphs", "algorithm": "Depth First Search", "example": "Write a program to traverse a graph using Depth First Search"},
+        {"data_structure": "Trie", "algorithm": "Autocomplete", "example": "Write a program to autocomplete a given string based on a given dictionary of words"}
+    ]
+
+    return dsa_list
