fc3dc6651195d89f3f3c8cb618b6c311c3c3e221
faizm10 <faizmustansar10@gmail.com>
2025-10-13 22:54:01 -0400

    optimized the solution

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -1,4 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        only_letters = "".join(ch for ch in s if ch.isalpha())
         return only_letters.lower() == only_letters.lower()[::-1]
 
