9dc021f9d125b0c204a56be4790a71cffbad421b
faizm10 <faizmustansar10@gmail.com>
2025-10-15 09:48:54 -0400

    optimized the solution

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -3,15 +3,21 @@ class Solution:
         nums.sort()
         res = []
         n = len(nums) # we calc the length of the list
-        i,j,k = 0,1,n-1
-        while i<j<k:
-            total = nums[i] + nums[j]+ nums[k]
-            #we want to check if there all equal to 0
-            if (total == 0):
-                res.append([nums[i],nums[j],nums[k]])
-            elif total > 0:
-                k-=1
-            elif total < 0:
-                j+=1
-            i+=1
+        for i,k in enumerate(nums):
+            if i>0 and nums[i-1] == nums[i]:
+                continue
+            l,r = i + 1, n - 1
+            while l < r:
+                total = k + nums[l]+ nums[r]
+                #we want to check if there all equal to 0
+                if (total == 0):
+                    res.append([k,nums[l],nums[r]])
+                    l+=1
+                    while nums == nums[i-1] and l<r:
+                        l+=1
+                elif total > 0:
+                    r-=1
+                elif total < 0:
+                    l+=1
         return res
+        
