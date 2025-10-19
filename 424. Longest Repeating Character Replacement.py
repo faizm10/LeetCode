87139e915ec225de43f57e0ebbdaeb9f38e6e62c
faizm10 <faizmustansar10@gmail.com>
2025-10-19 11:58:27 -0400

    problem #424: neetcode sol: learned it

--- 424. Longest Repeating Character Replacement.py
+++ 424. Longest Repeating Character Replacement.py
@@ -1,14 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        #intuituon:
-        freq = {}
-        best = 0
-        left = 0
-        right = 0
-        window = 0
-        for right, ch in enumerate(s): #tracks the index and value
-            freq[ch] = freq.get(ch, 0) + 1 
-            if (window - freq[ch] <=k):
-                window+=1
-        return window
-# https://stackoverflow.com/questions/8703496/hash-map-in-python
+        count = {}
+        res = 0
+        l = 0
+        maxf = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+
+            while (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+
+        return res
