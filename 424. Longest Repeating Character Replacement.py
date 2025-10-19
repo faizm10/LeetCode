efa411c63aa82d8f27ffe74ee4a3ca42800c44f2
faizm10 <faizmustansar10@gmail.com>
2025-10-19 11:48:49 -0400

    problem #424: first try

--- 424. Longest Repeating Character Replacement.py
+++ 424. Longest Repeating Character Replacement.py
@@ -1,4 +1,14 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         #intuituon:
-        
+        freq = {}
+        best = 0
+        left = 0
+        right = 0
+        window = 0
+        for right, ch in enumerate(s): #tracks the index and value
+            freq[ch] = freq.get(ch, 0) + 1 
+            if (window - freq[ch] <=k):
+                window+=1
+        return window
+# https://stackoverflow.com/questions/8703496/hash-map-in-python
