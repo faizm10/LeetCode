f51f74f0f6dd264a0bde83ea8d52178b83944fa8
faizm10 <faizmustansar10@gmail.com>
2025-10-20 09:17:35 -0400

    leetcode #567: complete

--- 567. Permutation in String.py
+++ 567. Permutation in String.py
@@ -1,5 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        return True
-        else: 
-            return False 
+        need = {}
+        m = len(s1)
+        n = len(s2)
+        if s1 > s2: 
+            return False
+        for ch in s1:
+            need[ch] = need.get(ch, 0) + 1
+
+        win = {} #set current window to see if it matches need
+        for ch in s2[:m]:
+            win[ch] = win.get(ch, 0) + 1
+        if win == need: return True
+
+        for i in range(m, n):
+            add = s2[i]
+            rem = s2[i - m]
+            win[add] = win.get(add, 0) + 1
+            win[rem] -= 1                      # shrink
+            if win[rem] == 0:
+                del win[rem]
+            if win == need: return True
+        return False
