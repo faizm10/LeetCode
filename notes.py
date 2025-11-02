65f277011de57f7c490f1884b6c7e9e4fcf542b4
faizm10 <faizmustansar10@gmail.com>
2025-11-02 13:13:13 -0500

    added some notes

--- notes.py
+++ notes.py
@@ -10,3 +10,19 @@ empty = len(stack) == 0
 from collections import deque
 stack = deque()
 stack.append(x); x = stack.pop()
+
+#
+#to find max depth of binary tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0                      # base (nodes)
+        L = self.maxDepth(root.left)
+        R = self.maxDepth(root.right)
+        return 1 + max(L, R)
