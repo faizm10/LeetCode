764d6f2e416218c4693cc204580f0a9369bbcddc
faizm10 <faizmustansar10@gmail.com>
2026-02-11 20:07:41 -0500

    updated code

--- 1. Two Sums.py
+++ 1. Two Sums.py
@@ -1,10 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # we need to find two numbers that add up to the target
-        #init the 1st and 2nd indices
-        
-        #run through the loop now
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]    
+        seen = {}
+
+        for i, n in enumerate(nums):
+            need = target - n
+
+            if need in seen:
+                return [seen[need],i]
+            seen[n] = i
+
+        return []
