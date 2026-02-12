29cf349faf69cfa43c76e6c48c979b2cf99cdaee
faizm10 <faizmustansar10@gmail.com>
2026-02-11 20:44:05 -0500

    updated code

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,10 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #we want to find anagrams for all of em, group the anagrams, etc
-        d = defaultdict(list)
-        for x in strs:
-            key = "".join(sorted(x))
-            #print(key)
-            d[key].append(x)
-            
-        return list(d.values())
+        groups = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+            if key not in groups:
+                groups[key]=[]
+            groups[key].append(word)
+
+        return list(groups.values())
