0a7b86a132dbfc19fc4c91dc065222ddc40b3162
faizm10 <faizmustansar10@gmail.com>
2026-02-11 20:15:14 -0500

    updated code

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -1,8 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        arr = set() # init the set
-        for i in nums: # checking every number in nums (list)
-            if i in arr: # if index is in the set, it already exists
-                return True 
-            arr.add(i) # if not, it is unique so we add it to the set
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
         return False
